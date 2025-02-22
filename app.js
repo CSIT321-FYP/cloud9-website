ce8cf1bca580aedd737ec6feb9d7f34013c20fbf
Atharva Khole <atharvakhole26@gmail.com>
2025-02-22 11:51:33 +0800

    Fix: CORS

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const routerMiddleware = require('./middleware/routerMiddleware')
-const errorMiddleware = require('./middleware/errorMiddleware');
-const passport = require('passport');
-const session = require('express-session');
-const cors = require('cors')
+const path = require("path");
+const routerMiddleware = require("./middleware/routerMiddleware");
+const errorMiddleware = require("./middleware/errorMiddleware");
+const passport = require("passport");
+const session = require("express-session");
+const cors = require("cors");
 
-app.use(cors())
+// Allow CORS from your frontend domain
+const corsOptions = {
+  origin: "https://cloud9app.site",
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
+app.use(cors(corsOptions));
 
 // Load environment variables
-require('dotenv').config({
-    override: true,
-})
+require("dotenv").config({
+  override: true,
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,24 +27,23 @@ app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 
 // Set EJS as the templating engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use('/views/assets', express.static('./views/assets/'))
-app.use('/img', express.static('./views/assets/img'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use("/views/assets", express.static("./views/assets/"));
+app.use("/img", express.static("./views/assets/img"));
 
-
-app.get('/', (req, res, next) => {
-    res.render('index')
-})
-app.get('/login', (req, res, next) => {
-    res.render('loginPage')
-})
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+app.get("/login", (req, res, next) => {
+  res.render("loginPage");
+});
 
 // Set middleware
-app.use('/', routerMiddleware)
-app.use('/', errorMiddleware)
+app.use("/", routerMiddleware);
+app.use("/", errorMiddleware);
 
 // Start server
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
